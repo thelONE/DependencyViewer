ce365146500622936f2995aa31a1e0423a2a6fca
thelONE <lidelong.li@gmail.com>
2014-12-19 22:36:59 +0900

    2
    following the chapter 6

---
+++ html/js/controllers/productListController.js
@@ -0,0 +1,13 @@
+﻿angular.module("sportsStore")
+    .controller("productListCtrl", function ($scope, $filter) {
+        
+        var selectedCategoroy = null;
+
+        $scope.selectCategory = function (newCategory) { 
+        selectedCategory = newCategory;
+        }
+        
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null || product.category == selectedCategory; 
+        } 
+    });
