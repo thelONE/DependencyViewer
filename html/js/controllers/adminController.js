1cf6ebca5f6be50d84b030dc7d2f159a36ba4c66
thelONE <lidelong.li@gmail.com>
2014-12-24 16:53:39 +0900

    Chapter 8

---
+++ html/js/controllers/adminController.js
@@ -0,0 +1,22 @@
+﻿angular.module("sportsStoreAdmin")
+.constant("authUrl", "https://api.parse.com/1/login")
+.run(function ($http) {
+    $http.defaults.headers.common["X-Parse-Application-Id"] = "DTRfVWS6peQI8XzYTiYihJwsNf3XAKfB5uPSVz8D";
+    $http.defaults.headers.common["X-Parse-REST-API-Key"] = "yQv7NI23DFf4ZAJ9rhRGuUUtSd4QYTTm69WcgKKh";
+})
+.controller("authCtrl", function ($scope, $http, $location, authUrl) {
+    
+    $scope.authenticate = function (user, pass) {
+        $http.get(authUrl, {
+            username: user,
+            password: pass
+        }).success(function () {
+            $scope.$broadcast("sessionToken", data.sessionToken);
+            $http.defaults.headers.common["X-Parse-Session-Token"] = data.sessionToken;
+            $location.path("/main");
+        }).error(function (response) {
+            $scope.authenticationError = response.error || response; 
+        });
+    }
+
+});
--- html/js/controllers/sportsStore.js
+++ html/js/controllers/sportsStore.js
@@ -1,11 +1,14 @@
 angular.module("sportsStore")
 .constant("dataUrl", "https://api.parse.com/1/classes/Products/")
+.constant("orderUrl", "https://api.parse.com/1/classes/Orders/")
 .run(function ($http) {
     $http.defaults.headers.common["X-Parse-Application-Id"] = "DTRfVWS6peQI8XzYTiYihJwsNf3XAKfB5uPSVz8D";
     $http.defaults.headers.common["X-Parse-REST-API-Key"] = "yQv7NI23DFf4ZAJ9rhRGuUUtSd4QYTTm69WcgKKh";
 })
-.controller("sportsStoreCtrl", function($scope, $http, dataUrl){
+.controller("sportsStoreCtrl", function($scope, $http, $location, dataUrl, orderUrl, cart){
+    
     $scope.data = {};
+    
     $http.get(dataUrl)
     .success(function (data) {
         $scope.data.products = data.results;
@@ -13,6 +16,21 @@ angular.module("sportsStore")
     .error(function (error) {
         $scope.data.error = error;
     });
+
+    $scope.sendOrder = function (shippingDetails) {
+        var order = angular.copy(shippingDetails);
+        order.products = cart.getProducts();
+        $http.post(orderUrl, order)
+        .success(function (data) {
+            $scope.data.orderId = data.objectId;
+            cart.getProducts().length = 0;
+        })
+        .error(function (error) { 
+            $scope.data.orderError = error;
+        }).finally(function () { 
+            $location.path("/complete");
+        });
+    }
 });
 
 // Test link
