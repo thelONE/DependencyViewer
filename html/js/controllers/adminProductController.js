e58531905f93a58eb527d5fdd0f35a6de583f1ba
thelONE <lidelong.li@gmail.com>
2014-12-25 23:44:22 +0900

    Chapter 8

---
+++ html/js/controllers/adminProductController.js
@@ -0,0 +1,11 @@
+﻿angular.module("sportsStoreAdmin")
+.constant("productUrl", "https://api.parse.com/1/classes/Products/")
+.run(function ($http) {
+    $http.defaults.headers.common["X-Parse-Application-Id"] = "DTRfVWS6peQI8XzYTiYihJwsNf3XAKfB5uPSVz8D";
+    $http.defaults.headers.common["X-Parse-REST-API-Key"] = "yQv7NI23DFf4ZAJ9rhRGuUUtSd4QYTTm69WcgKKh";
+})
+.controller("productCtrl", function ($scope, $http, $resource, productUrl) {
+    
+    
+     
+});
