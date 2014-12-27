33472d5890895038bfd361e71958919205a095ec
thelONE <lidelong.li@gmail.com>
2014-12-27 18:28:38 +0900

    End of Chapter 8

--- html/js/controllers/adminProductController.js
+++ html/js/controllers/adminProductController.js
@@ -6,6 +6,53 @@
 })
 .controller("productCtrl", function ($scope, $http, $resource, productUrl) {
     
-    
+    $scope.$on("sessionToken", function (sessionToken) {
+        $http.defaults.headers.common["X-Parse-Session-Token"] = sessionToken; 
+    });
+
+    function getData(data, headers) { 
+        return JSON.parse(data).results;
+    }
      
+    $scope.productsResource = $resource(productUrl + ":id", { id: "@objectId" }, {
+        query: { method: "GET", isArray:true, transformResponse:getData },
+        update: { method: "PUT"}
+    });
+
+    $scope.listProducts = function () { 
+        $scope.products = $scope.productsResource.query();
+    }
+
+    $scope.deleteProduct = function (product) {
+        product.$delete().then(function () { 
+            $scope.products.splice($scope.products.indexOf(product), 1);
+        });
+    }
+
+    $scope.createProduct = function (product) {
+        new $scope.productsResource(product).$save().then(function (response) {
+            response.$get().then(function (newProduct) {
+                $scope.products.push(newProduct);
+                $scope.editedProduct = nil;
+            })
+        });
+    }
+
+    $scope.updateProduct = function (product) {
+        var pCopy = {};
+        angular.copy(product, pCopy);
+        pCopy.$update().then(function () { 
+            $scope.editedProduct = null;
+        })
+    }
+
+    $scope.startEdit = function (product) { 
+        $scope.editedProduct = product;
+    }
+
+    $scope.cancelEdit = function () { 
+        $scope.editedProduct = null;
+    }
+
+    $scope.listProducts();
 });
